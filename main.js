73bc3664b2560bcf0bbc4145c925c54850992e22
lucasferreiral96 <lucasssferreira.l@outlook.com>
2022-11-14 21:34:53 -0300

    Calculadora finalizada.

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 function insert(num){
-    document.getElementsByClassName('resultado')[0].innerHTML = num;
+    let numero = document.getElementsByClassName('resultado')[0].innerHTML;
+    document.getElementsByClassName('resultado')[0].innerHTML = numero + num;
+}
+
+function clean(){
+
+
+document.getElementsByClassName('resultado')[0].innerHTML = "";
+
+
+}
+
+function back(){
+    let resultado = document.getElementsByClassName('resultado')[0].innerHTML;
+    document.getElementsByClassName('resultado')[0].innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    let resultado = document.getElementsByClassName('resultado')[0].innerHTML;
+    if(resultado){
+        document.getElementsByClassName('resultado')[0].innerHTML = eval(resultado);
+
+    }else{
+        document.getElementsByClassName('resultado')[0].innerHTML = "Insira algum numero.";
+
+    }
+
 }
